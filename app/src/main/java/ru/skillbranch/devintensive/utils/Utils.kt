034b0a9cd5f08d6f5c660604aa2631c090c9132b
shokred <shokred@yandex.ru>
2019-07-02 22:24:25 +0700

    Utils.transliteration

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -29,4 +29,61 @@ object Utils {
             }
         }
     }
+
+    private fun String.toUpperFirst(): String {
+        return this[0].toUpperCase() + this.substring(1)
+    }
+
+    fun transliteration(payload: String?, divider: String = " "): String {
+        val stringBuilder = StringBuilder()
+        payload?.map {
+            if (it.isUpperCase()) {
+                mapTranslite(it, divider).toUpperFirst()
+            } else {
+                mapTranslite(it, divider)
+            }
+        }?.joinTo(stringBuilder, separator = "")
+
+        return stringBuilder.toString()
+    }
+
+    private fun mapTranslite(char: Char, divider: String): String {
+        return when (char.toLowerCase()) {
+            'а' -> "a"
+            'б' -> "b"
+            'в' -> "v"
+            'г' -> "g"
+            'д' -> "d"
+            'е' -> "e"
+            'ё' -> "e"
+            'ж' -> "zh"
+            'з' -> "z"
+            'и' -> "i"
+            'й' -> "i"
+            'к' -> "k"
+            'л' -> "l"
+            'м' -> "m"
+            'н' -> "n"
+            'о' -> "o"
+            'п' -> "p"
+            'р' -> "r"
+            'с' -> "s"
+            'т' -> "t"
+            'у' -> "u"
+            'ф' -> "f"
+            'х' -> "h"
+            'ц' -> "c"
+            'ч' -> "ch"
+            'ш' -> "sh"
+            'щ' -> "sh'"
+            'ъ' -> ""
+            'ы' -> "i"
+            'ь' -> ""
+            'э' -> "e"
+            'ю' -> "yu"
+            'я' -> "ya"
+            ' ' -> divider
+            else -> char.toString()
+        }
+    }
 }
