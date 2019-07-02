895915e6439d25ec3345aa1b5ce0f057ac160410
shokred <shokred@yandex.ru>
2019-07-02 22:55:43 +0700

    BaseMessage.AbstractFactory

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -1,2 +1,31 @@
 package ru.skillbranch.devintensive.models
 
+import java.util.*
+
+abstract class BaseMessage(
+    val id: String,
+    val from: User?,
+    val chat: Chat,
+    val isIncoming: Boolean = false,
+    val date: Date = Date()
+) {
+    abstract fun formatMessage(): String
+
+    companion object AbstractFactory {
+        var lastId = -1
+        fun makeMessage(
+            from: User?,
+            chat: Chat,
+            date: Date = Date(),
+            type: String = "text",
+            payload: Any?,
+            isIncoming: Boolean = false
+        ): String {
+            lastId++
+            return when (type) {
+                "text" -> TextMessage("$lastId", from, chat, date = date, text = payload as String, isIncoming = isIncoming).formatMessage()
+                else -> ImageMessage("$lastId", from, chat, date = date, image = payload as String, isIncoming = isIncoming).formatMessage()
+            }
+        }
+    }
+}
