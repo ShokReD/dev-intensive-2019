cff1fa9efdae25fcc691dcf43be37b3fdea422bb
shokred <shokred@yandex.ru>
2019-07-03 07:41:39 +0700

    Fix Date.kt

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,8 +1,8 @@
 package ru.skillbranch.devintensive.extensions
 
-import ru.skillbranch.devintensive.TimeUnits
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.abs
 
 fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
     val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
@@ -41,7 +41,7 @@ enum class TimeUnit(
             this.forms[0]
         } else if (count.rem(10) == 1L && count.div(10) != 1L) {
             "$count ${this.forms[0]}"
-        } else if (count in 2..4 && count.div(10) != 1L) {
+        } else if (count.rem(10) in 2..4 && count.div(10) != 1L) {
             "$count ${this.forms[1]}"
         } else {
             "$count ${this.forms[2]}"
@@ -55,7 +55,7 @@ fun Date.humanizeDiff(): String {
     val now = Date()
     var millis = now.time - this.time
     val after = this.after(now)
-    millis = Math.abs(millis)
+    millis = abs(millis)
 
     return when (millis) {
         in ONE_SECOND -> "только что"
@@ -72,11 +72,40 @@ fun Date.humanizeDiff(): String {
 }
 
 private val ONE_SECOND = LongRange(0, 1000)
-private val FEW_SECOND = LongRange(ONE_SECOND.endInclusive + 1, 45L * 1000)
-private val ONE_MINUTE = LongRange(FEW_SECOND.endInclusive + 1, 75L * 1000)
-private val FEW_MINUTE = LongRange(ONE_MINUTE.endInclusive + 1, 45L * 60 * 1000)
-private val ONE_HOUR = LongRange(FEW_MINUTE.endInclusive + 1, 75L * 60 * 1000)
-private val FEW_HOUR = LongRange(ONE_HOUR.endInclusive + 1, 22L * 60 * 60 * 1000)
-private val ONE_DAY = LongRange(FEW_HOUR.endInclusive + 1, 26L * 60 * 60 * 1000)
-private val FEW_DAY = LongRange(ONE_DAY.endInclusive + 1, 360L * 24 * 60 * 60 * 1000)
-private val ONE_YEAR = LongRange(FEW_DAY.endInclusive + 1, Long.MAX_VALUE)
+private val FEW_SECOND = LongRange(ONE_SECOND.last + 1, 45L * 1000)
+private val ONE_MINUTE = LongRange(FEW_SECOND.last + 1, 75L * 1000)
+private val FEW_MINUTE = LongRange(ONE_MINUTE.last + 1, 45L * 60 * 1000)
+private val ONE_HOUR = LongRange(FEW_MINUTE.last + 1, 75L * 60 * 1000)
+private val FEW_HOUR = LongRange(ONE_HOUR.last + 1, 22L * 60 * 60 * 1000)
+private val ONE_DAY = LongRange(FEW_HOUR.last + 1, 26L * 60 * 60 * 1000)
+private val FEW_DAY = LongRange(ONE_DAY.last + 1, 360L * 24 * 60 * 60 * 1000)
+private val ONE_YEAR = LongRange(FEW_DAY.last + 1, Long.MAX_VALUE)
+
+enum class TimeUnits(
+    private val forms: Array<String>
+) {
+    MILLISECOND(arrayOf("милисекунду", "милисекунды", "милисекунд")),
+    SECOND(arrayOf("секунду", "секунды", "секунд")),
+    MINUTE(arrayOf("минуту", "минуты", "минут")),
+    HOUR(arrayOf("час", "часа", "часов")),
+    DAY(arrayOf("день", "дня", "дней")),
+    WEEK(arrayOf("неделю", "недели", "недель")),
+    MONTH(arrayOf("месяц", "месяца", "месяцев")),
+    YEAR(arrayOf("год", "года", "лет"));
+
+    fun format(count: Long? = null, unknown: Boolean = false, after: Boolean = false): String {
+        val output = if (unknown) {
+            "несколько ${this.forms[2]}"
+        } else if (count == null) {
+            this.forms[0]
+        } else if (count.rem(10) == 1L && count.div(10) != 1L) {
+            "$count ${this.forms[0]}"
+        } else if (count in 2..4 && count.div(10) != 1L) {
+            "$count ${this.forms[1]}"
+        } else {
+            "$count ${this.forms[2]}"
+        }
+
+        return if (after) "через $output" else "$output назад"
+    }
+}
