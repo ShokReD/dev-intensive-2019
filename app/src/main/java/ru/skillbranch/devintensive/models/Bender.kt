a6106bfab0f8bfcaae365d4001e5a9369e6b4f43
shokred <shokred@yandex.ru>
2019-07-13 23:37:19 +0700

    init hometask 3 commit

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -0,0 +1,49 @@
+package ru.skillbranch.devintensive.models
+
+class Bender(var status: Status = Status.NORMAL, var question: Question = Question.NAME) {
+    fun askQuestion(): String {
+        return question.question
+    }
+
+    fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
+        return if (question.answers.contains(answer)) {
+            question = question.nextQuestion()
+            "Отлично - это правильный ответ\n${question.question}"
+        } else {
+            status = status.nextStatus()
+            "Это неправильный ответ!\n${question.question}"
+        } to status.color
+    }
+
+    enum class Status(val color: Triple<Int, Int, Int>) {
+        NORMAL(Triple(255, 255, 255)),
+        WARNING(Triple(255, 120, 0)),
+        DANGER(Triple(255, 60, 60)),
+        CRITICAL(Triple(255, 255, 0));
+
+        fun nextStatus(): Status {
+            return if (this.ordinal < values().lastIndex) {
+                values()[this.ordinal + 1]
+            } else {
+                values()[0]
+            }
+        }
+    }
+
+    enum class Question(val question: String, val answers: List<String>) {
+        NAME("Как меня зовут?", listOf("бендер", "bender")),
+        PROFESSION("Назови мою профессию", listOf("сгибальщик", "bender")),
+        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")),
+        BIRTHDAY("Когда меня создали?", listOf("2993")),
+        SERIAL("Мой серийный номер", listOf("2716057")),
+        IDLE("На этом все, вопросов больше нет", listOf());
+
+        fun nextQuestion(): Question {
+            return if (ordinal < values().lastIndex) {
+                values()[ordinal + 1]
+            } else {
+                return values()[values().lastIndex]
+            }
+        }
+    }
+}
